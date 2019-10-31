69a54465691adf1ca5ca8bcd12eed555fba4f118
AlexHardDiskDrive <45855343+AlexHardDiskDrive@users.noreply.github.com>
2019-10-31 10:58:16 +0300

    Add button that create new row in table and add little changes in html

--- Bots/urls.py
+++ Bots/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('createBot/', CreateBot.as_view(), name='create_bot_url'),
     path('updateBot/<int:bot_id>/', UpdateBot.as_view(), name='update_bot_url'),
     path('deleteBot/<int:bot_id>/', DeleteBot.as_view(), name='delete_bot_url'),
-    path('createBot', CreateFileBot.as_view(), name='create_bot_url')
+    path('createBot', CreateFileBot.as_view(), name='create_bot_file_url')
 ]
